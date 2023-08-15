21b552d58dd0415f0bec5e83045e731ae22d7252
VIRESH SINGH <you@example.com>
2023-08-15 22:21:11 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+import{ Button } from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+      <h1> E-Comm Project</h1>
+      <button>Normal</button> <br/>
+      <br/>
+      <Button>bootstrap</Button>
+    </div>
+  );
+}
+
+export default App;
